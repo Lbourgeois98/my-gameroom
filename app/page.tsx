84cc3fa5db0bc71c386b5729b8c43867fccabfd4
Lbourgeois98 <73048129+Lbourgeois98@users.noreply.github.com>
2025-10-30 19:03:00 -0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import DepositForm from '@/components/DepositForm';
+
+export default function Page() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-800 via-black to-pink-900">
+      <DepositForm />
+    </main>
+  );
+}
